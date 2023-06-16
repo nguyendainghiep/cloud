7b4305ca749eda10df4db33c649fd9aff2f5e924
Le Nguyen Huy Hoang <hoanglnhgcc210076@fpt.edu.vn>
2023-06-16 15:13:04 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,4 +1,6 @@
 <?php
         $Connect = mysqli_connect("pfw0ltdr46khxib3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","fwu9no04dbu312al","chppz80ot7o0ybtf","ina709owwfh27bn1") or die ("Lỗi".mysqli_error($Connect));
+        //$Connect = mysqli_connect("mysql://fwu9no04dbu312al:chppz80ot7o0ybtf@pfw0ltdr46khxib3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/ina709owwfh27bn1")
         mysqli_query($Connect,'SET NAME "utf8"')
+        //mysqli_close($Connect)
 ?>
